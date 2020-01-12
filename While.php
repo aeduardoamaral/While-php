7502f3153ea3eb554df226b6073ad90fc7fc8566
Antonio Nascimento <aeduardoamaral@gmail.com>
2020-01-12 19:51:55 -0300

    Add files via upload

---
+++ While.php
@@ -0,0 +1,23 @@
+<?php
+$i=0;
+$n=100;
+$v=array($n);
+while($i<$n){
+	echo "Valor da variável i: $i<br>";
+	$i=$i+1;
+
+}
+
+echo "<br/><hr/><br/>";
+
+$g=0;
+while($g<$n){
+	
+	echo "Valor g: $g<br>";
+	
+	$g=$g+1;	
+}
+
+
+
+?>
